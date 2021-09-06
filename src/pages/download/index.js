30944cbf81be9eb36da207a65af9c5f35cdbf855
Raji Sarafadeen Sanjo <omosanjos77@gmail.com>
2021-09-06 19:50:32 +0100

    second test zc chat mobile view page

--- src/pages/download/index.js
+++ src/pages/download/index.js
@@ -1,48 +1,85 @@
-import { ReactComponent as GreenCircle} from '../images/circle.svg';
-import { ReactComponent as Rectangle} from '../images/downloadRectangle.svg';
+
+// import { ReactComponent as Rectangle} from '../images/downloadRectangle.svg';
 import { ReactComponent as Googleplay} from '../images/googleplay.svg';
 import { ReactComponent as Apple} from '../images/apple.svg';
+
+import React from 'react'
+// import Footer from '../../components/externalPagesComponents/Footer/Footer'
+// import Header from '../../components/externalPagesComponents/Header'
+// import styles from '../download/styles/download.module.css'
+
+
 // import Googleplay from '../images/Google_Play-Icon-Logo.wine.png';
-import { ReactComponent as Downarow} from '../images/downarow.svg';
-import { ReactComponent as Logo} from '../images/zurilogo.svg';
-import { ReactComponent as Facebook} from '../images/facebook.svg';
-import { ReactComponent as Instagram} from '../images/instagram.svg';
-import { ReactComponent as Twitter} from '../images/twitter.svg';
-import { ReactComponent as Linkedin} from '../images/linkedin.svg';
+ import RectangleOverlay from '../images/rectangle.png';
+ import Circle from '../images/circleimg.png';
+ import Logo from '../images/ZuriLogo.png';
+import Downarow from '../images/chevrondown.png';
+import Facebook from '../images/facebook.png';
+import Instagram from '../images/instagram.png';
+import Twitter from '../images/twitter.png';
+import Linkedin from '../images/linkedin.png';
+// import { ReactComponent as Facebook} from '../images/facebook.svg';
+// import { ReactComponent as Instagram} from '../images/instagram.svg';
+// import { ReactComponent as Twitter} from '../images/twitter.svg';
+// import { ReactComponent as Linkedin} from '../images/linkedin.svg';
 import styles from '../../styles/Download.module.css'
-// import { Topbar } from '../../components/Topbar'
+// import Header from '../../components/externalPagesComponents/Header';
+
 
 const Download = () => {
+
   return (
-  <div  className={styles.downloadWrapper}>
-    {/* <Topbar/> */}
-<div className={styles.downloadTitle}>
-    <h1>Zuri Chat Download</h1> 
-    <p>What's New</p>
-</div>
-<div  className={styles.rectangleContainer}>
-  <GreenCircle  className={styles.circle}/>
-  <Rectangle className={styles.rectangle}/>
-  </div>
-
-<div className={styles.downloadPc}>
-    <h2>Available on Windows 8 and higher</h2>
-     <a href="/">Downlod for Windows (64 bits)</a>
-     <h3>Download for Mac 0s X 10.10 and higher</h3>
-</div>
-
-  <div className={styles.devicetype}>
-        <h2>Zuri Chat for Mobile</h2>
-        <h3>Keep up with your team and hold meetings from your device</h3>
+    <div>
+    <section>
+        <div className={`${styles.container} container`}>
+          <h1 className={styles.h1}>Zuri Chat Download</h1>
+          <a href="/news" className={styles.link}>
+            What's New
+          </a>
+        </div>
+        <div className={styles.images}>
+          <img
+            className={styles.circle}
+            src={Circle}
+            alt="circle"
+          ></img>
+          <img
+            className={`${styles.screenshot}`}
+            src={RectangleOverlay}
+            alt="app screenshot"
+          ></img>
+          
+          <img
+            className={styles.circle2}
+            src={Circle}
+            alt="circle2"
+          ></img>
+        </div>
+        <div className={styles.buttondiv}>
+          <p className={styles.windows}>Available on windows 8 and higher</p>
+          <button className={styles.button1} type="submit">
+            Download for Windows (64 bits)
+          </button>
+          <button className={styles.button2} type="submit">
+            Download for Mac OS X 10.10 and higher
+          </button>
+          
+        </div>
+      </section>
+      <section className={styles.store}>
 
 
         <div className={styles.google_apple_Box}>
-              <div  className={styles.appleBox}>
+          <h1 className={styles.h1}>Zuri Chat for Mobile</h1>
+            <p className={styles.storep}>
+              Keep up with your team and hold meetings from your device
+            </p>
+              <div  className={styles.googleBox}>
           <Googleplay className={styles.googleplay}/>
-          <div className={styles.appleBox_text}>
-          <span className={styles.appleText1}>GET IT ON</span>
+          <div className={styles.googleBox_text}>
+          <small className={styles.googleText1}>GET IT ON</small>
           <br/>
-         <span className={styles.appleText}>Google Play</span>
+         <span className={styles.googleText}>Google Play</span>
          </div>
          </div>
        
@@ -55,41 +92,36 @@ const Download = () => {
          </div>
          </div>
         </div>
-        
-    </div>
+      </section>
+      <div className={styles.zurilogo}>
+        <span><img src={Logo} alt="zurilogo" /><span className={styles.zuri_logo_text}>Zuri Chat</span></span>
+      </div>
+      <footer>
+        <div className={styles.footertext}><p>WHY ZURI CHAT</p> <img src={Downarow} alt="down arrow" /></div>
+        <div className={styles.footertext}><p>PRODUCTS</p><img src={Downarow} alt="down arrow" /></div>
+        <div className={styles.footertext}><p>PRICING</p><img src={Downarow} alt="down arrow" /></div>
+        <div className={styles.footertext}><p>RESOURCES</p><img src={Downarow} alt="down arrow" /></div>
 
-
-<div className={styles.footer}>
-  
-     <div className={styles.zurilogo}>
-          <div><Logo className={styles.zclogoBottom}/><span>Zuri Chat</span></div>
-    </div>
-
-    <div className={styles.zcdescription}>
-      <p>WHY ZURI CHAT  <Downarow/> </p>
-      <p>PRODUCT <Downarow/></p>
-      <p>PRICING <Downarow/></p>
-      <p>RESOURCES <Downarow/></p>
-    </div>
-
-    <div  className={styles.zc_footer_nav}>
-      <p>Privacy</p>
-      <p>Terms</p>
-      <p>Help center</p>
-      <p>Contact Us</p>
-    </div>
-
-    <div className={styles.socialMedia}>
-      <Facebook className={styles.facebook}/>
-      <Instagram className={styles.instagram}/>
-      <Twitter/>
-      <Linkedin/>
-    </div>
-</div>
-    <p className={styles.copyright}>&copy 2021 zuri,Team Aristotle. All Rights Reserved.</p>
-    
+        <div className={styles.footerlinks}>
+          <a href="/">Privacy</a>
+          <a href="/">Terms</a>
+          <a href="/">Help Center</a>
+          <a href="/">Contact US</a>
+        </div>
+            <div className={styles.socio}>
+              <p><img src={Facebook} alt="facebook" /></p>
+              <p><img src={Instagram} alt="instagram" /></p>
+              <p><img src={Twitter} alt="twitter" /></p>
+              <p><img src={Linkedin} alt="linkedin" /></p>
+            </div>
+      </footer>
+      <p className={styles.copyright}><span dangerouslySetInnerHTML={{ "__html": "&copy;" }} />2021 zuri,Team Aristotle. All Rights Reserved.</p>
   </div>
   )
 }
 
 export default Download
+
+
+    
+  
